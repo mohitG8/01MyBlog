f724f3e385bb684120d71f958363549c25a6f544
Mohit Ghanchi <mohitghanchi020@gmail.com>
2024-05-14 10:39:28 +0530

    commited

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,23 +3,17 @@ import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
-    
-  return (
-    <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-2'>
-        <h2
-            className='text-xl font-bold'
-            >{title}</h2>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                 />
 
-            </div>
-            
-        </div>
-    </Link>
-  )
+return (
+  <Link to={`/post/${$id}`} className='block w-64 h-76'>
+    <div className='w-full h-full bg-gray-100 rounded-xl p-4 flex flex-col justify-between'>
+      <div className='mb-4'>
+        <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='w-full h-48 object-cover rounded-xl' />
+        <h2 className='text-xl font-bold mt-2'>{title}</h2>
+      </div>
+    </div>
+  </Link>
+)
 }
-
 
 export default PostCard
--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -33,9 +33,12 @@ export default function Post() {
     };
 
     return post ? (
-        <div className="py-8">
+        <div className="py-4">
             <Container>
-                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
+            <div className="w-full mb-6">
+                    <h1 className="text-2xl font-bold">{post.title}</h1>
+                </div>
+                <div className="w-full flex justify-center mb-4 relative border rounded-xl p-4 ">
                     <img
                         src={appwriteService.getFilePreview(post.featuredImage)}
                         alt={post.title}
@@ -55,9 +58,7 @@ export default function Post() {
                         </div>
                     )}
                 </div>
-                <div className="w-full mb-6">
-                    <h1 className="text-2xl font-bold">{post.title}</h1>
-                </div>
+                
                 <div className="browser-css">
                     {parse(post.content)}
                     </div>
@@ -65,3 +66,5 @@ export default function Post() {
         </div>
     ) : null;
 }
+
+ 
